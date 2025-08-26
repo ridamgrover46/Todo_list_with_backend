dc676e35716a4821c96901c36f33c57a877618d8
ridamgrover46 <ridamgrover14@gmail.com>
2025-08-26 13:35:08 +0530

    Update Register.jsx ok

--- todo/Frontend/src/pages/Register.jsx
+++ todo/Frontend/src/pages/Register.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"; 
 
 function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+   const navigate = useNavigate(); 
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -22,7 +24,6 @@ function Register() {
     }
   };
 
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form onSubmit={handleRegister} className="bg-white p-6 rounded shadow-md w-80">
@@ -37,4 +38,3 @@ function Register() {
 }
 
 export default Register;
-
